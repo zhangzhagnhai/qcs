bfa0e8846c9188beed295b4736bb9fe6415a4a6b
zhangzhanghai <32005651+zhangzhagnhai@users.noreply.github.com>
2018-07-05 09:45:31 +0800

    新版本更新

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -3,6 +3,7 @@
  */
 var host='http://laravel.hcsoo.com/api';
 host='https://www.hcsoo.com/api'
+host='http://6620da8d.ngrok.io/api'
  //host='http://192.168.1.108:8081/api'
 //host='http://192.168.1.117:8081/api'
 var shareHref='https://www.hcsoo.com/mp/index.html'
@@ -12,5 +13,15 @@ var strToJson=function(str){
 }
 //http://laravel.hcsoo.com/api/wx/wxApi
 
-export {host,shareHref,strToJson}
+var toPrice=function (num) {
+  var num = (num || 0).toString();
+  var result = '';
+  while (num.length > 3) {
+    result = ',' + num.slice(-3) + result;
+    num = num.slice(0, num.length - 3);
+  }
+  if (num) { result = num + result; }
+  return result;
+}
+export {host,shareHref,strToJson,toPrice}
 
